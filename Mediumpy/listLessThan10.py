497360610ae32c116fdeaac2db83691346141bf0
GurberGink <gurbergink@gmail.com>
2026-01-29 16:54:01 +0000

    Add list filtering functionality based on user input

---
+++ Mediumpy/listLessThan10.py
@@ -0,0 +1,12 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+b = []
+a.append(11)
+test = int(input("Enter a value 1-10: "))
+for num in a:
+    if num < test:
+        b.append(num)
+        b.sort()
+print(b)
+print()
+
+
