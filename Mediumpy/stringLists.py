d95f4e43765278f07656fc9e91f7082985ced007
GurberGink <gurbergink@gmail.com>
2026-01-30 16:04:58 +0000

    Add palindrome checker script with user input and feedback

---
+++ Mediumpy/stringLists.py
@@ -0,0 +1,25 @@
+import os
+from time import sleep
+
+os.system("clear")
+
+while True:
+    string = str(input("\nPlease enter a palindrome "))
+    p = string[::-1] 
+
+    if string == p:
+            os.system("clear")
+            sleep(1)
+            print("True, this is in fact a palindrome")
+            break
+    else:
+        os.system("clear")
+        sleep(1)
+        print("False, this is in fact not a palindrome. Please try again :)")
+            
+        
+    
+     
+    
+
+    
