eb4df107dbf18b5725dcad5db9041b4b1ce75eb2
GurberGink <240234520@aston.ac.uk>
2026-02-02 15:45:02 +0000

    Add guessing game implementation with user input and replay option

---
+++ Hardpy/guessingGameOne.py
@@ -0,0 +1,62 @@
+import random 
+import os
+from time import sleep
+ 
+yesOrNo = ["Yes", "No"]
+status = True 
+answer = random.randint(1,9)
+
+os.system("clear")
+
+while status:
+    while True:
+        try:
+            guess = int(input("Please enter a value 1 - 9 \n\t"))
+            
+            if guess >= 1 and guess <= 9:
+                break
+            
+            else:
+                print("The value must be between 1 - 9")
+                
+        except ValueError:
+            os.system("clear")
+            print("Please enter a valid integer. ") 
+            sleep(1)
+            os.system("clear")            
+            
+    if guess == answer:
+        print("\nCongratulations that was correct")
+    
+    elif guess < answer:
+        print("\nThe guess is too low")
+        sleep(1)
+        os.system("clear")
+    elif guess > answer:
+        print("\nThe guess was too high")
+        sleep(1)
+        os.system("clear")
+    
+    if guess == answer:
+        sleep(1)
+        os.system("clear")
+        check = input("Would you like to play again? \n\t Yes|No\n\n\t  ").capitalize()
+    
+        while check not in yesOrNo:
+            print("This input is invalid. Please try again.")
+            check = input("Would you like to play again? \n\t Yes|No\n\n\t  ").capitalize()
+            
+        if check == "No":
+            os.system("clear")
+            print("Thank you for playing :)")
+            sleep(3)
+            status = False
+            
+        elif check == "Yes":
+            os.system("clear")
+            print("Okay lets play again :)")
+            sleep(3)
+            os.system("clear")
+            answer = random.randint(1,9)
+        
+
